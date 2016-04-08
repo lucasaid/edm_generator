3e8cc7c362e98b0820cd029ac1605e2d38cabcb5
Chris Lucas <christopher.lucas11@gmail.com>
2016-04-08 10:31:26 +1000

    feat(env): Added sample environment file

---
+++ .env.sample.js
@@ -0,0 +1,16 @@
+// Copy this file to .env.js and change the staging details to where you would like to ftp to.
+module.exports = {
+
+  AWS_BUCKET: "",
+  AWS_ACCESS_KEY: "",
+  AWS_SECRET: "",
+  AWS_BUCKET_URL: "''",
+
+  LITMUS_USER: "",
+  LITMUS_PASS: "",
+  LITMUS_URL: "",
+
+  // 'chrome', 'firefox', 'safari', 'ie'
+  // IE, Windows and Linux have not been tested, fully working on OSX.
+  DEFAULT_BROWSER: "chrome",
+};
